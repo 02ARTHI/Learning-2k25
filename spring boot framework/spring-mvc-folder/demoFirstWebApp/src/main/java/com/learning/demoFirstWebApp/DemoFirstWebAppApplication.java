e7cc58163cf79dd443c6397f45965d77bea81b3a
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2025-03-04 23:45:45 +0530

    Add files via upload

---
+++ spring boot framework/spring-mvc-folder/demoFirstWebApp/src/main/java/com/learning/demoFirstWebApp/DemoFirstWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.demoFirstWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstWebAppApplication.class, args);
+	}
+
+}
---
+++ spring boot framework/spring-mvc-folder/demoFirstWebApp/src/main/java/com/learning/demoFirstWebApp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.learning.demoFirstWebApp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoFirstWebAppApplication.class);
+	}
+
+}
