d1b54824b403f3c1533adf40644e57202b911a3a
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2025-06-04 23:58:48 +0530

    Add files via upload

--- Problems/Stream API Problems/NullClass.java
+++ Problems/Stream API Problems/NullClass.java
@@ -2,13 +2,22 @@ package Streams.mayLast;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class NullClass {
 
 	public static void main(String[] args) {
-		List<String> s= Arrays.asList("africa","dubai","india","asia","japan","america","russia"," ",null);
-		s.stream().filter(e->e!=null&& (!e.isEmpty())).forEach(System.out::println);
+		List<String> s = Arrays.asList("africa", "dubai", "india", "asia", "japan", "america", "russia", " ", null);
+		s.stream().filter(e -> e != null && (!e.isEmpty())).forEach(System.out::println);
 
+		System.out.println("--------------");
+		
+		Map<Character,List<String>> str=s.stream().collect(Collectors.groupingBy(e->e.charAt(0)));
+   str.forEach((key,value)-> {
+	         System.out.println(key);
+		   value.forEach(System.out::println);
+		   });
 	}
 
 }
