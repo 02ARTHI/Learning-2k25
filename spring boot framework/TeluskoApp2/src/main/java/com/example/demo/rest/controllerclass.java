7cc067d6dc5086aae502cbb675909c4079348f77
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2025-06-15 21:54:31 +0530

    Add files via upload

---
+++ spring boot framework/TeluskoApp2/src/main/java/com/example/demo/rest/controllerclass.java
@@ -0,0 +1,22 @@
+package com.example.demo.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class controllerclass {
+	
+	@GetMapping("/info")
+	public String getMethodName() {
+		
+		RestTemplate rs = new RestTemplate();
+		ResponseEntity<String> forObject = rs.getForEntity("http://localhost:9090/get", String.class);
+		String bodyString=forObject.getBody();
+		
+		return bodyString+" , Hi from telusko app 2";
+	}
+	
+
+}
